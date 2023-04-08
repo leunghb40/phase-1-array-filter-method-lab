61eebfff06cdce580a4d3e130fe1327f34b49f08
Qing Bin Liang <leunghb@hotmail.com>
2023-04-08 16:02:47 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+const drivers = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland'},
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+];
+
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+ }
+
+const name = 'Bobby';
+const matchingDrivers = matchName(drivers, name);
+console.log(matchingDrivers);
